2d996e2ed299134843cc0ed27277089fbae794d2
Ab <104671142+alex-blag@users.noreply.github.com>
2023-11-25 20:52:29 +0200

    feat: added Spring Boot project (#1)
    * feat: added Spring Boot project
    * feat: dev test using GitHub Actions
    
    ---------
    
    Co-authored-by: ab <ab@>

---
+++ src/test/java/ru/cobp/backend/CobpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cobp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
