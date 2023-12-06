02ae21a95c940d4cdb1ef5296877914d4d4e1145
Ab <104671142+alex-blag@users.noreply.github.com>
2023-12-06 12:59:49 +0200

    feat: API spec generation with Swagger and SpringDoc (#15)

--- src/test/java/ru/cobp/backend/CobpBackendApplicationTests.java
+++ src/test/java/ru/cobp/backend/CobpBackendApplicationTests.java
@@ -1,13 +1,12 @@
 package ru.cobp.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CobpBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
