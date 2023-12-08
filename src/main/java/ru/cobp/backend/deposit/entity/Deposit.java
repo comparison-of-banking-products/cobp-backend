20e5310cd6d34023b58894209649e3aeaa301a3d
ab <ab@>
2023-12-08 16:02:01 +0200

    feat: add querydsl

--- src/main/java/ru/cobp/backend/deposit/entity/Deposit.java
+++ src/main/java/ru/cobp/backend/deposit/entity/Deposit.java
@@ -12,7 +12,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import ru.cobp.backend.banks.entity.Bank;
+import ru.cobp.backend.bank.entity.Bank;
 import ru.cobp.backend.currency.entity.Currency;
 
 @Entity
