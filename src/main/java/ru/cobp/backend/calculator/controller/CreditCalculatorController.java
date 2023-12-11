6c5fe701ed493f5742cfa726c0f4cb6aef89ab43
Ab <104671142+alex-blag@users.noreply.github.com>
2023-12-11 09:58:46 +0200

    feat: add calculators

--- src/main/java/ru/cobp/backend/calculator/controller/CreditCalculatorController.java
+++ src/main/java/ru/cobp/backend/calculator/controller/CreditCalculatorController.java
@@ -6,11 +6,17 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.cobp.backend.credit.dto.CreditDto;
+import ru.cobp.backend.calculator.dto.AnnuityCreditCalculatorDto;
+import ru.cobp.backend.calculator.dto.DifferentialCreditCalculatorDto;
+import ru.cobp.backend.calculator.service.AnnuityCreditCalculator;
+import ru.cobp.backend.calculator.service.DifferentialCreditCalculator;
 
 @Tag(
         name = "Кредитный калькулятор",
@@ -18,23 +24,51 @@ import ru.cobp.backend.credit.dto.CreditDto;
 )
 @RestController
 @RequestMapping(path = "/v1/calculators/credit")
+@RequiredArgsConstructor
 public class CreditCalculatorController {
 
+    private final AnnuityCreditCalculator annuityCreditCalculator;
+
+    private final DifferentialCreditCalculator differentialCreditCalculator;
+
     @Operation(
-            summary = "Найти кредит",
-            description = "Конечная точка для поиска кредита с наинизшей ставкой"
+            summary = "Рассчитать аннуитетный кредит",
+            description = "Конечная точка для расчета аннуитетного кредита с наинизшей ставкой"
     )
     @ApiResponses(value = {@ApiResponse(
             responseCode = "200",
-            description = "Кредит найден",
+            description = "Аннуитетный кредит рассчитан",
             content = {@Content(
                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = CreditDto.class)
+                    schema = @Schema(implementation = AnnuityCreditCalculatorDto.class)
             )}
     )})
-    @GetMapping
-    public void getCredit() {
-        throw new UnsupportedOperationException("Not implemented yet");
+    @GetMapping("/annuity")
+    public AnnuityCreditCalculatorDto calculateAnnuityCredit(
+            @RequestParam @Positive int amount,
+            @RequestParam @Positive int term
+    ) {
+        return annuityCreditCalculator.calculate(amount, term);
+    }
+
+    @Operation(
+            summary = "Рассчитать дифференцированный кредит",
+            description = "Конечная точка для расчета дифференцированного кредита с наинизшей ставкой"
+    )
+    @ApiResponses(value = {@ApiResponse(
+            responseCode = "200",
+            description = "Дифференцированный кредит рассчитан",
+            content = {@Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = DifferentialCreditCalculatorDto.class)
+            )}
+    )})
+    @GetMapping("/differential")
+    public DifferentialCreditCalculatorDto calculateDifferentialCredit(
+            @RequestParam @Positive int amount,
+            @RequestParam @Positive int term
+    ) {
+        return differentialCreditCalculator.calculate(amount, term);
     }
 
 }
