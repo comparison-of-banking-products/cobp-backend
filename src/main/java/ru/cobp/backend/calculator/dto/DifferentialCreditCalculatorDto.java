bce6552cce6bf680aa944b067f052b736b9e63d0
Ab <104671142+alex-blag@users.noreply.github.com>
2023-12-11 11:01:37 +0200

    feat: add calculators

--- src/main/java/ru/cobp/backend/calculator/dto/DifferentialCreditCalculatorDto.java
+++ src/main/java/ru/cobp/backend/calculator/dto/DifferentialCreditCalculatorDto.java
@@ -1,9 +1,27 @@
 package ru.cobp.backend.calculator.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "Результат расчета дифференцированного кредита")
 @Data
 @NoArgsConstructor
 public class DifferentialCreditCalculatorDto {
+
+    @Schema(description = "Наинизшая доступная ставка")
+    private Double rate;
+
+    @Schema(description = "Ежемесячный платеж")
+    private Double monthlyPayment;
+
+    @Schema(description = "Начисленные проценты")
+    private Double totalInterest;
+
+    @Schema(description = "Общая выплата")
+    private Double totalPayments;
+
+    @Schema(description = "Id кредита ставка которого использовалась в расчетах")
+    private Long creditId;
+
 }
