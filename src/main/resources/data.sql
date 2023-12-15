619c5ac0d94cad0965bafbcebaffcb56cb7eeed1
Ab <104671142+alex-blag@users.noreply.github.com>
2023-12-15 10:33:02 +0200

    feat: add Tinkoff deposits into data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,39 @@
 INSERT INTO banks (bic, name, legal_entity, description, logo, url)
 VALUES
     (1, 'ban4ok-1', 'ban4ok-1-legalnyy', 'dlya vas svezhenkiy tovarchik, a dlya nas - solidnyy navarchik', 'ban4ok-1-logo', 'ban4ok-1-url'),
-    (2, 'ban4ok-2', 'ban4ok-2-legalnyy', 'u kogo net nalichnykh, budet rydat neprilichno', 'ban4ok-2-logo', 'ban4ok-2-url');
+    (2, 'ban4ok-2', 'ban4ok-2-legalnyy', 'u kogo net nalichnykh, budet rydat neprilichno', 'ban4ok-2-logo', 'ban4ok-2-url'),
+    (044525974, 'Тинькофф Банк', '?', 'Онлайн-экосистема, основанная на финансовых и лайфстайл-услугах', '?', 'https://www.tinkoff.ru');
 
 INSERT INTO currencies (num, code, currency)
 VALUES (643, 'RUB', 'Russian ruble');
 
 INSERT INTO deposits (bank_bic, name, product_url, is_active, currency_num, amount_min, amount_max, term, rate)
 VALUES
-    (1, 'deposit-5%', 'deposit-5%-url', true, 643, 10000, 1000000, 12, 5.0),
-    (2, 'deposit-7%', 'deposit-7%-url', true, 643, 10000, 1000000, 12, 7.0);
+    (044525974, 'Вклад 14,5%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 3, 14.5),
+    (044525974, 'Вклад 14,5%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 4, 14.5),
+    (044525974, 'Вклад 14,5%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 5, 14.5),
+
+    (044525974, 'Вклад 14,1%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 6, 14.1),
+    (044525974, 'Вклад 14,1%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 7, 14.1),
+    (044525974, 'Вклад 14,1%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 8, 14.1),
+    (044525974, 'Вклад 14,1%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 9, 14.1),
+    (044525974, 'Вклад 14,1%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 10, 14.1),
+    (044525974, 'Вклад 14,1%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 11, 14.1),
+
+    (044525974, 'Вклад 13,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 12, 13.0),
+    (044525974, 'Вклад 13,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 13, 13.0),
+    (044525974, 'Вклад 13,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 14, 13.0),
+    (044525974, 'Вклад 13,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 15, 13.0),
+    (044525974, 'Вклад 13,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 16, 13.0),
+    (044525974, 'Вклад 13,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 17, 13.0),
+
+    (044525974, 'Вклад 12,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 18, 12.0),
+    (044525974, 'Вклад 12,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 19, 12.0),
+    (044525974, 'Вклад 12,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 20, 12.0),
+    (044525974, 'Вклад 12,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 21, 12.0),
+    (044525974, 'Вклад 12,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 22, 12.0),
+    (044525974, 'Вклад 12,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 23, 12.0),
+    (044525974, 'Вклад 12,0%', 'https://www.tinkoff.ru/savings/deposit/', true, 643, 50000, 30000000, 24, 12.0);
 
 INSERT INTO credits (bank_bic, name, product_url, is_active, currency_num, amount_min, amount_max, term, rate)
 VALUES
