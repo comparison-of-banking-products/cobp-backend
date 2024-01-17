b4d78f54c77de9fd4cc3623e7a28da693e1713bc
Ab <104671142+alex-blag@users.noreply.github.com>
2024-01-17 09:44:44 +0200

    fix: remove unused import

--- src/main/java/ru/cobp/backend/controller/calculator/CalculatorController.java
+++ src/main/java/ru/cobp/backend/controller/calculator/CalculatorController.java
@@ -16,7 +16,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
