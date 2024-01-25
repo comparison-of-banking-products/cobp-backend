465b965367cba4f32863f4bbb9e70313b27f0873
Ab <104671142+alex-blag@users.noreply.github.com>
2024-01-25 10:15:42 +0200

    fix: rename Bic

--- src/main/java/ru/cobp/backend/validation/constraints/Bic.java
+++ src/main/java/ru/cobp/backend/validation/constraints/Bic.java
@@ -2,14 +2,20 @@ package ru.cobp.backend.validation.constraints;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import ru.cobp.backend.common.Constants;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@BicLength(message = "{bic.length.invalid}")
-@BicOnlyDigits(message = "{bic.digits.invalid}")
+@Size.List({
+        @Size(min = Constants.BIC_LENGTH, message = "{bic.length.invalid}"),
+        @Size(max = Constants.BIC_LENGTH, message = "{bic.length.invalid}")
+})
+@Pattern(regexp = Constants.ONLY_DIGITS_PATTERN, message = "{bic.digits.invalid}")
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = {})
