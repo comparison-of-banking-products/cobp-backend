179764b2e2ab9c5e1d283094a00d9dd9652e78f9
mdemidkin <mdemidkin@yandex.ru>
2024-01-25 16:08:56 +0100

    fix: resolve conflicts

--- src/main/java/ru/cobp/backend/exception/ExceptionHandlerController.java
+++ src/main/java/ru/cobp/backend/exception/ExceptionHandlerController.java
@@ -8,14 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -55,15 +52,6 @@ public class ExceptionHandlerController {
     public ErrorResponseDto handle(final DuplicateException ex) {
         log.error(ex.getMessage(), ex);
         return new ErrorResponseDto(LocalDateTime.now(), ex.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public List<ErrorResponseDto> handle(final MethodArgumentNotValidException ex) {
-        List<ObjectError> errors = ex.getBindingResult().getAllErrors();
-        List<ErrorResponseDto> response = new ArrayList<>();
-        for (var e : errors) response.add(new ErrorResponseDto(LocalDateTime.now(), e.getDefaultMessage()));
-        return response;
     }
 
     @ExceptionHandler
